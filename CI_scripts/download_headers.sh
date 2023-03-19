8bf6a0ef2ebd63582c4eddbe10c4362c79cf8488
rA9stuff <barisulascukur@gmail.com>
2023-03-19 15:10:02 +0300

    a

---
+++ CI_scripts/download_headers.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/libimobiledevice/libimobiledevice.git
+git clone https://github.com/libimobiledevice/libirecovery.git
+git clone https://github.com/libimobiledevice/libusbmuxd.git
+git clone https://github.com/libimobiledevice/libplist.git
+git clone https://github.com/libimobiledevice/libimobiledevice-glue.git
